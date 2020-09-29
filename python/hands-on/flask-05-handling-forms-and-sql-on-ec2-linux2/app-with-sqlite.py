30bdb8a192f7c3fad41d460de158ee494b1b37e0
mehmet <drmehmet510@gmail.com>
2020-09-29 11:09:17 +0300

    uP TO DATE

--- python/hands-on/flask-05-handling-forms-and-sql-on-ec2-linux2/app-with-sqlite.py
+++ python/hands-on/flask-05-handling-forms-and-sql-on-ec2-linux2/app-with-sqlite.py
@@ -97,5 +97,6 @@ def add_email():
 
 # Add a statement to run the Flask application which can be reached from any host on port 80.
 if __name__ == '__main__':
-    # app.run(debug=True)
-   app.run(host='0.0.0.0', port=80)
+    db.create_all()
+    app.run(debug=True)
+   #app.run(host='0.0.0.0', port=80)
