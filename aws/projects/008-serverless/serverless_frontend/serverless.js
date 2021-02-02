ce6b2c7c53bc58b60a078e6854f81054d9c7b692
mehmet <drmehmet510@gmail.com>
2021-02-02 19:57:40 +0300

    up to date

---
+++ aws/projects/008-serverless/serverless_frontend/serverless.js
@@ -0,0 +1,54 @@
+
+var API_ENDPOINT = 'https://bqdu3u07xl.execute-api.us-east-1.amazonaws.com/beta/pet';
+// if correct it should be similar to https://somethingsomething.execute-api.us-east-1.amazonaws.com/prod/petcuddleotron
+
+var errorDiv = document.getElementById('error-message')
+var successDiv = document.getElementById('success-message')
+var resultsDiv = document.getElementById('results-message')
+
+// function output returns input button contents
+function waitSecondsValue() { return document.getElementById('waitSeconds').value }
+function messageValue() { return document.getElementById('message').value }
+function emailValue() { return document.getElementById('email').value }
+function phoneValue() { return document.getElementById('phone').value }
+
+function clearNotifications() {
+    errorDiv.textContent = '';
+    resultsDiv.textContent = '';
+    successDiv.textContent = '';
+}
+
+// When buttons are clicked, these are run passing values to API Gateway call
+document.getElementById('bothButton').addEventListener('click', function(e) { sendData(e, 'both'); });
+document.getElementById('emailButton').addEventListener('click', function(e) { sendData(e, 'email'); });
+document.getElementById('smsButton').addEventListener('click', function(e) { sendData(e, 'sms'); });
+
+
+function sendData (e, pref) {
+    e.preventDefault()
+    clearNotifications()
+    fetch(API_ENDPOINT, {
+        headers:{
+            "Content-type": "application/json"
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            waitSeconds: waitSecondsValue(),
+            preference: pref,
+            message: messageValue(),
+            email: emailValue(),
+            phone: phoneValue()
+        }),
+        mode: 'cors'
+    })
+    .then((resp) => resp.json())
+    .then(function(data) {
+        console.log(data)
+        successDiv.textContent = 'Submitted. But check the result below!';
+        resultsDiv.textContent = JSON.stringify(data);
+    })
+    .catch(function(err) {
+        errorDiv.textContent = 'Oops! Error Error:\n' + err.toString();
+        console.log(err)
+    });
+};
