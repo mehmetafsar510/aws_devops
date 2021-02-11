74e907b6cedd3481d9b7fc0af3a0c7b2db5d9253
mehmet <drmehmet510@gmail.com>
2021-02-11 10:47:54 +0300

    up to date

--- aws/projects/002-roman-numerals-converter/001-roman-numerals-converter/app.py
+++ aws/projects/002-roman-numerals-converter/001-roman-numerals-converter/app.py
@@ -12,19 +12,19 @@ def convert(decimal_num):
 
 @app.route('/', methods=['GET'])
 def main_get():
-    return render_template('index.html', developer_name='Serdar', not_valid=False)
+    return render_template('index.html', developer_name='Mehmet', not_valid=False)
 
 @app.route('/', methods=['POST'])
 def main_post():
     alpha = request.form['number']
     if not alpha.isdecimal():
-        return render_template('index.html', developer_name='Serdar', not_valid=True)
+        return render_template('index.html', developer_name='Mehmet', not_valid=True)
 
     number = int(alpha)
     if not 0 < number < 4000:
-        return render_template('index.html', developer_name='Serdar', not_valid=True)
+        return render_template('index.html', developer_name='Mehmet', not_valid=True)
 
-    return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='Serdar')
+    return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='Mehmet')
 
 if __name__ == '__main__':
     #app.run(debug=True)
